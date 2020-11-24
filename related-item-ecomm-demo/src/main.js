a8564afa838780c779496c854cdd707d965c77da
Hugo Valla <hugo_valla@hotmail.fr>
2020-11-24 16:53:25 +0100

    Product related classified by brand first then by genre with ratings and basic infos

--- related-item-ecomm-demo/src/main.js
+++ related-item-ecomm-demo/src/main.js
@@ -5,27 +5,43 @@ import { configure, hits, EXPERIMENTAL_configureRelatedItems } from 'instantsear
 
 const searchClient = algoliasearch(
   'GENYJWQIK2',
-  'd7a56394e700ad117ef483c12bc04481'
+  'a847d02d26f1276fbb0281a7e51ee8a5'
 );
 
 const search = instantsearch({
-  indexName: 'perso_movies_carousel',
+  indexName: 'e_commerce_transformed',
   searchClient,
 });
 
 const referenceHit = {
-  title: 'The Imitation Game',
-  image: 'https://image.tmdb.org/t/p/w154/ntZGfHt4d73A9fDD4KUN4nbDQlq.jpg',
-  color: '#192229',
-  popularity_score: 108.4335114104872,
-  actors: ['Benedict Cumberbatch', 'Keira Knightley', 'Matthew Goode'],
-  genre: ['History', 'Drama', 'Thriller', 'War'],
-  ongoing_watch: [],
-  tmdb_id: 205596,
-  views_last_7_days: 596898,
-  days_to_expire: 44,
-  objectID: '439434880',
+  objectID: "8532557",
+  brand: 'Apple',
+  image: '"https://cdn-demo.algolia.com/bestbuy-0118/8532557_sb.jpg"',
+  name: '"Apple - MacBook Pro with Retina display - 13.3" Display - 8GB Memory - 128GB Flash Storage - Silver"',
+  description: "With fifth-generation Intel Core processors, the latest graphics, and faster flash storage, the incredibly advanced MacBook Pro with Retina display moves even further ahead in performance and battery life.* *Compared with the previous generation.",
+  categories: [ "Name Brands", "Apple", "Mac" ],
+  popularity: 21442,
+  free_shipping: true,
+  hierarchicalCategories: {
+    lvl0: "Name Brands",
+    lvl1: "Name Brands > Apple",
+    lvl2: "Name Brands > Apple > Mac",
+    },
+    onSale: false,
+    price: 1299.99,
+    price_range: "500 - 2000",
+    seller: "RandomSeller#7",
+    type: "Apple",
+    url: "https://api.bestbuy.com/click/-/8532557/pdp",
+    rating: 4,
+    ratingsNumber: 397,
+    newPrice: 1299.99
 };
+
+
+
+
+
 
 
 // Add the widgets
@@ -37,30 +53,96 @@ search.addWidgets([
   EXPERIMENTAL_configureRelatedItems({
     hit: referenceHit,
     matchingPatterns: {
-      genre: { score: 3 },
-      actors: { score: 2 },
+      brand: { score: 3 },
+      categories: { score: 2 },
     },
   }),
   hits({
     container: '.carousel-container',
     templates: {
       item: (hit) => `
-        <img src="${hit.image}" alt="${hit.title}">
-        <div class="overlay">
-          <h3>${hit.title}</h3>
-          <ul>
-            ${hit.genre
-              .map(
-                (genre) => `
-              <li>${genre}</li>
-            `
-              )
-              .join('')}
-          </ul>
+      <div class="card-wrapper">
+          <div class="img-hit">
+            <img src="${hit.image}" align="left" alt="${hit.name}" class="hit-img" />
+          </div>
+          <div class="hit-name">
+          ${hit.brand}
+          </div>
+          <div class="hit-description">
+          ${hit.name}
+          </div>
+          <div class="hit-rating-price">
+            <div class="hit-ratings">${ratings(hit.rating)} <p> (${hit.ratingsNumber})</p></div>
+            <div class="hit-price">$${hit.price}</div>
+          </div>
         </div>
       `
+    
     }
   }),
 ]);
+
+const ratings = (hit) =>{
+  switch (hit) {
+    case hit = 0: 
+      return `<div>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      </div>` 
+      break;
+    case hit = 1: 
+      return `<div>
+      <i class="fas fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      </div>` 
+      break;
+    case hit = 2: 
+      return `<div>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      </div>` 
+      break;
+    case hit = 3: 
+      return `<div>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      </div>` 
+      break;
+    case hit = 4: 
+      return `<div>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="far fa-star"></i>
+      </div>` 
+      break;
+    case hit = 4: 
+      return `<div>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      </div>` 
+      break;
+    default:
+      return 'No ratings yet for this product'
+      console.log('Erreur cas inconnu');  
+  }
+  
+  }
 
 search.start();
