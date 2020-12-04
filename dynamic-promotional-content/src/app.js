96dd92e21b572961bb7e727be6e272bc2c872969
Haroen Viaene <hello@haroen.me>
2020-12-04 16:17:01 +0100

    add optional translation

--- dynamic-promotional-content/src/app.js
+++ dynamic-promotional-content/src/app.js
@@ -9,11 +9,27 @@ const searchClient = algoliasearch(
 const search = instantsearch({
   indexName: 'instant_search',
   searchClient,
-  // onStateChange(...args) {
-  //   console.log(args);
-  // },
+  onStateChange({ uiState, setUiState }) {
+    const bannerIndexId = 'instant_search_promotions';
+    const mainIndexId = 'instant_search';
+    const mainUiState = uiState[mainIndexId];
+
+    setUiState({
+      ...uiState,
+      // set the refinements you wan to make optional here
+      [bannerIndexId]: {
+        optionalRefinement: {
+          ...mainUiState.refinementList,
+          ...mainUiState.menu,
+          // set the attribute to [] if you don't want it to be inherited
+          price: [],
+        },
+      },
+    });
+  },
 });
 
+// TODO: remove this, it was just for highlighting
 function taggedTemplateNoop(strings, ...keys) {
   const lastIndex = strings.length - 1;
   return (
@@ -79,21 +95,62 @@ const templates = {
   },
 };
 
-// TODO: turn filters into optional filtesr
-// TODO: finish the three layouts
-search.addWidgets([
-  instantsearch.widgets.searchBox({
-    container: '#searchbox',
-  }),
-  instantsearch.widgets
+// custom widget which translates regular refinements to optional refinements
+function optionalRefinementTranslator() {
+  return {
+    render() {},
+    getWidgetSearchParameters(state, { uiState }) {
+      const { optionalRefinement = {} } = uiState;
+      return Object.keys(optionalRefinement).reduce(
+        (partialState, attribute) => {
+          const values = optionalRefinement[attribute];
+          // refinementList uses array, menu value directly.
+          // this way we can consistently set the refinement
+          const valuesAsArray = Array.isArray(values) ? values : [values];
+
+          return partialState.setQueryParameters({
+            // remove every widget's refinement for this attribute
+            disjunctiveFacetsRefinements: {
+              ...partialState.disjunctiveFacetsRefinements,
+              [attribute]: [],
+            },
+            facetsRefinements: {
+              ...partialState.facetsRefinements,
+              [attribute]: [],
+            },
+            hierarchicalFacetsRefinements: {
+              ...partialState.facetsRefinements,
+              [attribute]: [],
+            },
+            numericRefinements: {
+              ...partialState.facetsRefinements,
+              [attribute]: [],
+            },
+
+            // add it again as an optional filter
+            optionalFilters: [
+              ...(partialState.optionalFilters || []),
+              ...valuesAsArray.map(value => `${attribute}:${value}`),
+            ],
+          });
+        },
+        state
+      );
+    },
+  };
+}
+
+function banner({ container }) {
+  return instantsearch.widgets
     .index({ indexName: 'instant_search_promotions' })
     .addWidgets([
       instantsearch.widgets.configure({
         page: 0,
         hitsPerPage: 1,
       }),
+      optionalRefinementTranslator(),
       instantsearch.widgets.hits({
-        container: '#banner',
+        container,
         templates: {
           item(item) {
             return templates[item.layout_type]?.(item);
@@ -101,7 +158,33 @@ search.addWidgets([
           empty: '',
         },
       }),
-    ]),
+    ]);
+}
+
+// TODO: finish the three layouts
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+  banner({ container: '#banner' }),
+  instantsearch.widgets.panel({ templates: { header: 'brand' } })(
+    instantsearch.widgets.refinementList
+  )({
+    container: '#refinement-list',
+    attribute: 'brand',
+  }),
+  instantsearch.widgets.panel({ templates: { header: 'categories' } })(
+    instantsearch.widgets.menu
+  )({
+    container: '#menu',
+    attribute: 'categories',
+  }),
+  instantsearch.widgets.panel({ templates: { header: 'price' } })(
+    instantsearch.widgets.rangeSlider
+  )({
+    container: '#range',
+    attribute: 'price',
+  }),
   instantsearch.widgets.hits({
     container: '#hits',
     templates: {
@@ -119,3 +202,5 @@ search.addWidgets([
 ]);
 
 search.start();
+
+window.search = search;
